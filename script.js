be670c20838577b2c1c735dbebd2298f8b80c6a0
shreya maheshwari <62559289+shreya12-37@users.noreply.github.com>
2021-08-12 10:47:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-$.getScript(“https://cdnjs.cloudflare.com/ajax/libs/jSignature/2.1.3/jSignature.min.js”).done(function(){
-    $("#signature").jSignature({'UndoButton':true});
-    $(document).ready(function() {
+$.getScript(“https://cdnjs.cloudflare.com/ajax/libs/jSignature/2.1.3/jSignature.min.js”)
+    .done(function(){
+        $("#signature").jSignature({'UndoButton':true});
+        $(document).ready(function() {
 
-        // Initialize jSignature
-        var $sigdiv = $("#signature").jSignature({'UndoButton':true});
-        
-        $('#click').click(function(){
-         // Get response of type image
-         var data = $sigdiv.jSignature('getData', 'image');
-        
-         // Storing in textarea
-         $('#output').val(data);
-        
-         // Alter image source 
-         $('#sign_prev').attr('src',"data:"+data);
-         $('#sign_prev').show();
+            // Initialize jSignature
+            var $sigdiv = $("#signature").jSignature({'UndoButton':true});
+
+            $('#click').click(function(){
+             // Get response of type image
+             var data = $sigdiv.jSignature('getData', 'image');
+
+             // Storing in textarea
+             $('#output').val(data);
+
+             // Alter image source 
+             $('#sign_prev').attr('src',"data:"+data);
+             $('#sign_prev').show();
         });
     });
 });
